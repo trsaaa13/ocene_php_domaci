df03a274344b6869d724811fede0cbef1909800d
trsaaa13 <makicaaa.2000@gmail.com>
2023-01-11 19:40:39 +0100

    Select za izbor operacija i forma za evidenciju ocena

---
+++ forme/nova_ocena.php
@@ -0,0 +1,23 @@
+<div id="div_nova_ocena" hidden>
+
+    <div class="div_element_forma">
+        <label>Ime i prezime</label>
+        <input type="text">
+    </div>
+
+    <div class="div_element_forma">
+        <label>Broj bodova</label>
+        <input type="number">
+    </div>
+
+    <div class="div_element_forma">
+        <label>Predmet</label>
+        <select></select>
+    </div>
+
+    <div class="div_element_forma">
+        <label>Nastavnik</label>
+        <select></select>
+    </div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <title>Evidencija kontrolnih/pismenih zadataka</title>
+</head>
+
+<body>
+    <div id="div_body">
+
+
+        <h1 id="h1_naslov">Sistem za evidenciju/pregled ocena kontrolnih/pismenih zadataka</h1>
+
+        <select id="select_izaberi">
+            <option value="0">Izaberi</option>
+            <option value="nova_ocena">Unesi novu ocenu</option>
+        </select>
+
+        <div id="div_holder">
+            <?php
+            require 'forme/nova_ocena.php';
+            ?>
+        </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
+<script>
+    $("#select_izaberi").change(function() {
+
+        if ($(this).val() == 'nova_ocena')
+            $('#div_nova_ocena').show()
+
+    });
+</script>
